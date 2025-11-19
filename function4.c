ca013cc80d216a03de09dc2108674deb80543475
nmcculloch <nmcculloch@tcu.edu>
2025-11-18 23:04:39 -0600

    did function4

---
+++ function4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sumAll(int count, ...) {
+va_list args;
+va_start(args, count);
+int sum = 0;
+for (int i = 0; i < count; i++) {
+sum += va_arg(args, int);
+}
+va_end(args);
+return sum;
+}
+int main() {
+srand(time(NULL));
+int a = rand() % 99 + 1;
+int b = rand() % 99 + 1;
+int c = rand() % 99 + 1;
+int d = rand() % 99 + 1;
+printf("4 random numbers: %d\n", sumAll(4, a, b, c, d));
+int e = rand() % 99 + 1;
+int f = rand() % 99 + 1;
+int g = rand() % 99 + 1;
+int h = rand() % 99 + 1;
+int i = rand() % 99 + 1;
+int j = rand() % 99 + 1;
+printf("6 random numbers: %d\n", sumAll(6, e, f, g, h, i, j));
+return 0;
+}
